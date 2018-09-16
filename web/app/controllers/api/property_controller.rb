ba4d81b5202f7501a12d84d2de2e0da0a93a611b
José Ribeiro <zeca@digup.io>
2018-09-16 07:31:47 -0300

    Created formated response.

--- web/app/controllers/api/property_controller.rb
+++ web/app/controllers/api/property_controller.rb
@@ -1,12 +1,34 @@
 module API
   class PropertyController < ApplicationController
   
+    #
+    # Endpoint responsible to filter properties near a
+    # specific location (5km), property_type and marketing_type.
+    # 
+    # Calling example: /api/properties?lng=-23.528874&lat=-47.466840&property_type=apartment&marketing_type=rent
     get '/api/properties' do
       request = Requests::Property::Get.new(params)
       request.validate!
 
       response = PropertyRepository.new.similar(request)
-      halt_data(200, response.results)
+      halt_data(200, format_property_results(response.results))
     end
+
+    private
+
+      # Apply the json format for results
+      def format_property_results(results)
+        results.map do | property |
+          {
+            house_number: property.house_number,
+            street: property.street,
+            city: property.city,
+            zip_code: property.zip_code,
+            lat: property.location["lat"],
+            lng: property.location["lon"],
+            price: property.price,
+          }
+        end
+      end
   end
 end
--- web/spec/controllers/api/property_controller_spec.rb
+++ web/spec/controllers/api/property_controller_spec.rb
@@ -21,20 +21,32 @@ describe API::PropertyController, elasticsearch: true do
       end
 
       context 'and are data matched' do
-        subject { get '/api/properties?lng=-23.528874&lat=-47.466840&property_type=apartment&marketing_type=sell' }
+        subject { get '/api/properties?lng=-23.528874&lat=-47.466840&property_type=apartment&marketing_type=rent' }
 
         before do
-          FactoryGirl.create(:property, :family_house, :rent, zip_code: '001', lng: -23.528874, lat: -47.466840) # beer caps (searching point!)
-          FactoryGirl.create(:property, :apartment,    :sell, zip_code: '002', lng: -23.527678, lat: -47.468569) # jack pub  (2km)
-          FactoryGirl.create(:property, :apartment,    :sell, zip_code: '003', lng: -23.568083, lat: -47.459705) # cervejaria bamberg (4.49km)
-          FactoryGirl.create(:property, :apartment,    :rent, zip_code: '004', lng: -23.505449, lat: -47.470245) # the crown pub (2.48km)
-          FactoryGirl.create(:property, :family_house, :rent, zip_code: '005', lng: -23.495320, lat: -47.435167) # bardolino (5.12km)
-          FactoryGirl.create(:property, :apartment,    :sell, zip_code: '006', lng: 13.4236807, lat: 52.5342963) # Berlin location
+          FactoryGirl.create(:property, :family_house, :rent, zip_code: '001', lng: -23.528874, lat: -47.466840, price: 1000) # beer caps (searching point!)
+          FactoryGirl.create(:property, :apartment,    :sell, zip_code: '002', lng: -23.527678, lat: -47.468569, price: 1000) # jack pub  (2km)
+          FactoryGirl.create(:property, :apartment,    :sell, zip_code: '003', lng: -23.568083, lat: -47.459705, price: 1000) # cervejaria bamberg (4.49km)
+          FactoryGirl.create(:property, :apartment,    :rent, zip_code: '004', lng: -23.505449, lat: -47.470245, price: 1000) # the crown pub (2.48km)
+          FactoryGirl.create(:property, :family_house, :rent, zip_code: '005', lng: -23.495320, lat: -47.435167, price: 1000) # bardolino (5.12km)
+          FactoryGirl.create(:property, :apartment,    :sell, zip_code: '006', lng: 13.4236807, lat: 52.5342963, price: 1000) # Berlin location
           PropertyRepository.new.import_all!
         end
 
-        it { expect(subject.status).to eql(200) }
-        it { expect(JSON.parse(subject.body).count).to eq(2) }
+        it { expect(subject.status).to eql(200)              }
+        it { expect(JSON.parse(subject.body).count).to eq(1) }
+
+        it "should format the response" do
+          expect(JSON.parse(subject.body).first).to eq({
+            "house_number" => "123",
+            "street"       => "Street address",
+            "city"         => "Sorocaba",
+            "zip_code"     => "004",
+            "lat"          => "-47.470245",
+            "lng"          => "-23.505449",
+            "price"        => "1000.0"
+          })
+        end
       end
     end
   end

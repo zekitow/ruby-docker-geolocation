e7ccd32420175fa6778836e0f71f631888f33471
José Ribeiro Filho <zeca@digup.io>
2018-09-20 20:00:30 -0300

    Update shared_examples.rb

--- web/spec/shared_examples.rb
+++ web/spec/shared_examples.rb
@@ -1,8 +1,3 @@
-
-#
-# Exemplo usado para verificar se model e valido, quando é removido o attributo "parameter"
-# Exemplo: ./spec/models/player_spec.rb
-#
 shared_examples "model without required attribute" do |model, parameter|
 
   subject { model.new(model_attributes.except(parameter.to_sym)).valid? }
